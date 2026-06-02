a2e129cfbeb771def4adfca6f37192a072f571a7
Kam Wing Yiu <ctom3560@gmail.com>
2026-06-02 15:17:40 +0700

    cs220 assignment 2

--- cs220/src/assignments/assignment02/small_exercises.rs
+++ cs220/src/assignments/assignment02/small_exercises.rs
@@ -7,17 +7,17 @@ const FAHRENHEIT_SCALE: f64 = 5.0 / 9.0;
 
 /// Converts Fahrenheit to Celsius temperature degree.
 pub fn fahrenheit_to_celsius(degree: f64) -> f64 {
-    todo!()
+    (degree - FAHRENHEIT_OFFSET) * FAHRENHEIT_SCALE
 }
 
 /// Capitalizes English alphabets (leaving the other characters intact).
 pub fn capitalize(input: String) -> String {
-    todo!()
+    input.to_ascii_uppercase()
 }
 
 /// Returns the sum of the given array. (We assume the absence of integer overflow.)
 pub fn sum_array(input: &[u64]) -> u64 {
-    todo!()
+    input.iter().sum()
 }
 
 /// Given a non-negative integer, say `n`, return the smallest integer of the form `3^m` that's
@@ -25,13 +25,25 @@ pub fn sum_array(input: &[u64]) -> u64 {
 ///
 /// For instance, up3(6) = 9, up3(9) = 9, up3(10) = 27. (We assume the absence of integer overflow.)
 pub fn up3(n: u64) -> u64 {
-    todo!()
+    let mut up3 = 1;
+    while up3 < n {
+        up3 *= 3;
+    }
+    up3
 }
 
 /// Returns the greatest common divisor (GCD) of two non-negative integers. (We assume the absence
 /// of integer overflow.)
 pub fn gcd(lhs: u64, rhs: u64) -> u64 {
-    todo!()
+    // Euclidean Algorithm
+    let mut a = lhs;
+    let mut b = rhs;
+    while b != 0 {
+        let temp = b;
+        b = a % b;
+        a = temp;
+    }
+    a
 }
 
 /// Returns the array of nC0, nC1, nC2, ..., nCn, where nCk = n! / (k! * (n-k)!). (We assume the
@@ -40,7 +52,16 @@ pub fn gcd(lhs: u64, rhs: u64) -> u64 {
 /// Consult <https://en.wikipedia.org/wiki/Pascal%27s_triangle> for computation of binomial
 /// coefficients without integer overflow.
 pub fn chooses(n: u64) -> Vec<u64> {
-    todo!()
+    // A row for 'n' has exactly (n + 1) elements
+    let mut row: Vec<u64> = vec![1; (n + 1) as usize];
+
+    for i in 1..=n as usize {
+        // Compute elements backwards to update the vector in place safely
+        for j in (1..i).rev() {
+            row[j] = row[j - 1] + row[j];
+        }
+    }
+    row
 }
 
 /// Returns the "zip" of two vectors.
@@ -48,5 +69,11 @@ pub fn chooses(n: u64) -> Vec<u64> {
 /// For instance, `zip(vec![1, 2, 3], vec![4, 5])` equals to `vec![(1, 4), (2, 5)]`. Here, `3` is
 /// ignored because it doesn't have a partner.
 pub fn zip(lhs: Vec<u64>, rhs: Vec<u64>) -> Vec<(u64, u64)> {
-    todo!()
+    let mut zip: Vec<(u64, u64)> = Vec::new();
+
+    for i in 0..lhs.len().min(rhs.len()) {
+        zip.push((lhs[i], rhs[i]));
+    }
+
+    zip
 }
